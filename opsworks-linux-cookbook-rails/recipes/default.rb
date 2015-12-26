2f48bab0fdd6913ae2e0a912e89b80ea7473858b
Anshul Sharma <optimisticanshul@gmail.com>
2015-12-26 15:12:47 +0530

    first commit

--- opsworks-linux-cookbook-rails/recipes/default.rb
+++ opsworks-linux-cookbook-rails/recipes/default.rb
@@ -1,10 +1,25 @@
 include_recipe 'build-essential'
 
 app = search(:aws_opsworks_app).first
+Chef::Log.info "Application Details: #{app.inspect}"
+
 app_path = "/srv/#{app['shortname']}"
 
-package node['rails-app']['mysql_package_name']
+file "/srv/id_rsa" do
+  content "#{app['app_source']['ssh_key']}"
+  mode 0600
+end
 
+file "/srv/chef_ssh_deploy_wrapper.sh" do
+  content  <<-EOF
+  #!/bin/sh
+  exec ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "/srv/id_rsa" "$@"
+  EOF
+  mode 0770
+end
+
+
+package node['rails-app']['mysql_package_name']
 package value_for_platform_family(debian: 'ruby-dev', rhel: 'ruby-devel')
 
 package 'git' do
@@ -19,9 +34,11 @@ application app_path do
   git app_path do
     repository app['app_source']['url']
     action :sync
+    revision app['app_source']['revision']
+    ssh_wrapper "/srv/chef_ssh_deploy_wrapper.sh"
   end
 
-  ruby_runtime '2.2'
+  ruby_runtime '2'
   ruby_gem 'rake'
 
   bundle_install do
@@ -31,7 +48,7 @@ application app_path do
 
   rails do
     database 'sqlite3:///db.sqlite3'
-    migrate true
+    migrate app['environment']['RAILS_MIGRATION'] == "true" ? true : false
   end
 
   unicorn do
